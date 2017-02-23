7bb00936fb46b41e7e76ad8252f02314c205a1af
skotep <skotep@gmail.com>
2017-02-23 16:38:16 -0600

    add example validation function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import { Provider } from 'react-redux'
+import createLogger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+
+import Reducer from './reducers'
+import Hello from './hello'
+
+const logger = createLogger()
+const store = createStore(Reducer, applyMiddleware(logger))
+
 render(
-    <div>Hello React!</div>,
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
     document.getElementById('app')
 )
---
+++ src/reducers.js
@@ -0,0 +1,17 @@
+import * as Actions from './actions'
+
+const Reducer = (state = {
+    text: 'hello world!',
+    message: '',
+}, action) => {
+    switch (action.type) {
+        case Actions.UPDATE_TEXT:
+            return { ...state, text: action.text, message: '' }
+        case Actions.ERROR:
+            return { ...state, message: action.message }
+        default:
+            return state
+    }
+}
+
+export default Reducer
